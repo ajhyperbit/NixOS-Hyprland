d3b86398b9939ec58b2b82fc4e3885868dc00f4a
installer <installer@gmail.com>
2024-10-29 12:28:37 -0500

    add custom flake back

--- flake.nix
+++ flake.nix
@@ -1,19 +1,39 @@
 {
-  description = "KooL's NixOS-Hyprland"; 
-  	
-  inputs = {
-  	nixpkgs.url = "nixpkgs/nixos-unstable";
-	#wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
-	hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # hyprland development
-	distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes"; 
-  	};
 
-  outputs = 
-	inputs@{ self,nixpkgs, ... }:
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      #url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager"; #unstable / master(?)
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      #inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes"; 
+    #inputs.flake-compat = {
+    #  url = "github:edolstra/flake-compat";
+    #  flake = false;
+    #};
+  };
+
+outputs = inputs@{
+  self,
+  nixpkgs,
+  home-manager,
+  ...
+  }:
     	let
       system = "x86_64-linux";
-      host = "NixOS-Hyprland";
-      username = "alice";
+      host = "nixos";
+      username = "ajhyperbit";
 
     pkgs = import nixpkgs {
        	inherit system;
@@ -23,19 +43,32 @@
       };
     in
       {
-	nixosConfigurations = {
+    nixosConfigurations = {
       "${host}" = nixpkgs.lib.nixosSystem rec {
-		specialArgs = { 
-			inherit system;
-			inherit inputs;
-			inherit username;
-			inherit host;
-			};
-	   		modules = [ 
+      system = "x86_64-linux";
+      specialArgs = {
+			  inherit system;
+			  inherit inputs;
+			  inherit username;
+			  inherit host;
+        inherit self;
+        };
+      modules = [
 				./hosts/${host}/config.nix 
-				inputs.distro-grub-themes.nixosModules.${system}.default
-				];
-			};
-		};
-	};
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ajhyperbit = { imports = [ ./config/home.nix ];};
+          home-manager.extraSpecialArgs = {inherit inputs self username;};
+          home-manager.backupFileExtension = "hm-bak";
+        }
+        #wayland.windowManager.hyprland {
+        #  enable = true;
+        #  package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        #}
+      ];
+      };
+    };
+  };
 }
