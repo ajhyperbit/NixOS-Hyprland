d6f13fed719391d370fc1b31ae938f514ad81b14
installer <installer@gmail.com>
2024-11-11 13:36:40 -0600

    modified:   hosts/nixtop/config.nix

--- hosts/nixtop/config.nix
+++ hosts/nixtop/config.nix
@@ -968,14 +968,14 @@ let
       enable = true;
       wlr.enable = true;
       extraPortals = with pkgs; [ 
-        #xdg-desktop-portal
-        xdg-desktop-portal-kde
-        xdg-desktop-portal-gtk
+        #xdg-desktop-portals
+        #xdg-desktop-portal-kde
+        #xdg-desktop-portal-gtk
       ];
       xdgOpenUsePortal = true;
     configPackages = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal
+      #pkgs.xdg-desktop-portal-gtk
+      #pkgs.xdg-desktop-portal
     ];
 
     config = {
