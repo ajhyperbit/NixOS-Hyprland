e704d99fe0895b408ccf6b1d8c3022fa9b6cf341
ajhyperbit <ajhyperbit@gmail.com>
2024-11-26 21:00:49 -0600

    create flake-update.sh to automate flake.lock updates

---
+++ flake-update.sh
@@ -0,0 +1,6 @@
+#Update flake.lock
+nix flake update
+#Add flake.lock to staged changes
+git add flake.lock
+#Commit the changes so that there isn't a "git tree dirty" warning
+git commit -m "update flake.lock"
