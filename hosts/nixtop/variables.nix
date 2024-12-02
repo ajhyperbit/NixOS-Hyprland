3448ebd6ad786fcf6416e5da70833457551f20aa
ajhyperbit <ajhyperbit@gmail.com>
2024-12-01 21:40:19 -0600

    revert variable.nix as there is no need for host variables

--- hosts/nixtop/variables.nix
+++ hosts/nixtop/variables.nix
@@ -13,6 +13,4 @@
   browser = "google-chrome-stable"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
   keyboardLayout = "us";
-
-  host = "nixtop";
 }
