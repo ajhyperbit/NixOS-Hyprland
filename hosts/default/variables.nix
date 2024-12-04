053ee3656c6d3564a2727fb190b86ccd3f61e195
ajhyperbit <ajhyperbit@gmail.com>
2024-12-04 12:42:13 -0600

    revert default changes

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -1,7 +1,7 @@
 {
   # Git Configuration ( For Pulling Software Repos )
-  gitUsername = "AJHyperBit";
-  gitEmail = "ajhyperbit@gmail.com";
+  gitUsername = "JaKooLit";
+  gitEmail = "ejhay.games@gmail.com";
 
   # Hyprland Settings
   extraMonitorSettings = "";
@@ -10,7 +10,7 @@
   clock24h = true;
 
   # Program Options
-  browser = "google-chrome-stable"; # Set Default Browser (google-chrome-stable for google-chrome)
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
   keyboardLayout = "us";
 }
