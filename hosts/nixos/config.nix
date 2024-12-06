e8efe4d0b59d038fbbeaf8bc2e3b189d8ed7d617
ajhyperbit <ajhyperbit@gmail.com>
2024-12-06 02:30:42 -0600

    add firewall rules

--- hosts/nixos/config.nix
+++ hosts/nixos/config.nix
@@ -36,7 +36,16 @@
   networking = {
     hostName = "${host}";
     interfaces.enp6s0.wakeOnLan.enable = true;
+  firewall = {
+  enable = true;
+    allowedTCPPorts = [ 47984 47989 47990 48010 ];
+    allowedUDPPortRanges = [
+      { from = 47998; to = 48000; }
+      { from = 8000; to = 8010; }
+    ];
+};
   };
+
   environment.systemPackages = with pkgs; [
     davinci-resolve-studio
   ];
