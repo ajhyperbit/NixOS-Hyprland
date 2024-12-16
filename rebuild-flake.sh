3790642fe50573a783d11f93982a9a2c602ba46d
ajhyperbit <ajhyperbit@gmail.com>
2024-12-16 15:52:45 -0600

    update rebuild-flake.sh with host varibale functionality

--- rebuild-flake.sh
+++ rebuild-flake.sh
@@ -14,17 +14,23 @@
 #    else 
 #        git pull || echo "git pull failed, please see log." ; exit 1 
 
-reswitch=${1:-}
+host=${1:-}
+reswitch=${2:-}
+
+if [ -z $host ]; then
+    echo "Script requires a agrument for a host name. Current host names are "nixos" or "nixtop.""
+    exit 1
+fi
 
 if [ -z $reswitch ]; then
-    echo "Script requires an agrument. Either switch, boot, test, build, dry-activate."
+    echo "Script requires a second agrument for how to rebuild. Either switch, boot, test, build, or dry-activate."
     exit 1
 fi
 
 echo "NixOS Rebuilding..."
 
 # Rebuild, output simplified errors, log trackebacks
-sudo nixos-rebuild $reswitch --upgrade --show-trace --flake .#nixos &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1) || grep -P -n "dotfiles\/nixos\/([a-zA-Z]+).nix:[0-9]+:[0-9]+|\/home\/ajhyperbit\/dotfiles\/nixos\/([a-zA-Z]+).nix" nixos-switch.log | sed 's/:[[:blank:]]*/: /'
+sudo nixos-rebuild $reswitch --upgrade --show-trace --flake .#$host &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1) || grep -P -n "dotfiles\/nixos\/([a-zA-Z]+).nix:[0-9]+:[0-9]+|\/home\/ajhyperbit\/dotfiles\/nixos\/([a-zA-Z]+).nix" nixos-switch.log | sed 's/:[[:blank:]]*/: /'
 
 #grab current generation number
 #current_tag=$(nixos-rebuild list-generations | grep current | grep -Eo '[0-9]+' | head -1)
