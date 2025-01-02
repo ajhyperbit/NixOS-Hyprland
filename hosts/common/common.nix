826d1522e6b3b09762fe5326773d1b6caabe0300
ajhyperbit <ajhyperbit@gmail.com>
2025-01-02 14:46:56 -0600

    add: proton-ge-bin to programs.steam.extraCompatPackages

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -835,7 +835,7 @@ in {
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       extraCompatPackages = with pkgs; [
-        #proton-ge-bin
+        proton-ge-bin
         #steamtinkerlaunch
       ];
       gamescopeSession.enable = true;
