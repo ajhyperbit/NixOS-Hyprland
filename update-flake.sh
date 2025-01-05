b3a03dbae28f039a8d46675e7e4178cc70af41ca
ajhyperbit <ajhyperbit@gmail.com>
2025-01-04 22:43:08 -0600

    fix: make commit message better

--- update-flake.sh
+++ update-flake.sh
@@ -3,4 +3,4 @@ nix flake update
 #Add flake.lock to staged changes
 git add flake.lock
 #Commit the changes so that there isn't a "git tree dirty" warning
-git commit -m "update flake.lock"
+git commit -m "chore: update flake.lock"
