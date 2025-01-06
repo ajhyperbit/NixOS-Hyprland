94098b43f3eab4c675f5a765dcbec3ed51a0ab9d
ajhyperbit <ajhyperbit@gmail.com>
2025-01-06 00:21:47 -0600

    fix: remove duplicated definition

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -172,7 +172,6 @@ in {
 
   environment.sessionVariables = {
     XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_DIRS = "/etc/xdg";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
