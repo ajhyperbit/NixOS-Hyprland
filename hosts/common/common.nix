781af80f53dd248b72d9389a55851555db86e9a4
ajhyperbit <ajhyperbit@gmail.com>
2025-01-06 00:19:57 -0600

    feat: add XDG Base Directory Specification sessionVariables https://specifications.freedesktop.org/basedir-spec/latest/

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -170,6 +170,14 @@ in {
     };
   };
 
+  environment.sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_DIRS = "/etc/xdg";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
+
   #ANCHOR Packages
 
   environment.systemPackages =
