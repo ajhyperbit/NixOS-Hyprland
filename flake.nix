f9294ec0dc3e0e048e1654db1249809ad622d574
ajhyperbit <ajhyperbit@gmail.com>
2025-01-09 21:58:09 -0600

    Revert "testing/debugging: changing Hyprland input"
    This reverts commit f0adc0f83227755b23597121f67b04f12978cb0d.

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
     };
 
     hyprland = {
-      #url = "github:hyprwm/Hyprland";
-      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      url = "github:hyprwm/Hyprland";
+      #url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
