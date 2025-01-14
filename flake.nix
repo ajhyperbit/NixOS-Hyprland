b8d13ee20bba1965fc3de7c352556398b995abc9
ajhyperbit <ajhyperbit@gmail.com>
2025-01-13 22:58:08 -0600

    more making iso stuff

--- flake.nix
+++ flake.nix
@@ -442,7 +442,6 @@
             modulesPath,
             ...
           }: {
-            #imports = [(modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")];
           })
           nixos-hardware.nixosModules.framework-11th-gen-intel
           fw-fanctrl.nixosModules.default
