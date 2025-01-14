9a87d42f3282430573a00537a4f8b3a9a7ebbf6b
ajhyperbit <ajhyperbit@gmail.com>
2025-01-13 23:12:59 -0600

    make home manager stuff work right

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -444,10 +444,6 @@
     algorithm = "zstd";
   };
 
-  home = {
-    stateVersion = lib.mkForce "${stateVersion-host-iso}";
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
---
+++ hosts/iso/home.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  options,
+  username,
+  lib,
+  stateVersion-host-iso,
+  ...
+}: {
+  imports = [
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+    stateVersion = lib.mkDefault "${stateVersion-host-iso}";
+  };
+
+  stylix = {
+    #enable = true;
+    autoEnable = true; #default is true
+    polarity = "dark";
+    cursor = {
+      name = "Oxygen-Zion";
+      package = pkgs.oxygen;
+      size = 24;
+    };
+  };
+}
