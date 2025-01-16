c1dbfdd3baa9c13ce67714159010cf60b10fb52d
ajhyperbit <ajhyperbit@gmail.com>
2025-01-16 13:51:28 -0600

    feat: change to latest nvidia packages

--- modules/nvidia-drivers.nix
+++ modules/nvidia-drivers.nix
@@ -96,15 +96,17 @@ in {
 
         nvidiaSettings = true;
 
+        package = config.boot.kernelPackages.nvidiaPackages.latest
+
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "555.58.02";
-          sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-          sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-          openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-          settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-          persistencedSha256 = lib.fakeSha256;
-        };
+        #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #  version = "555.58.02";
+        #  sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+        #  sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+        #  openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        #  settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        #  persistencedSha256 = lib.fakeSha256;
+        #};
       };
 
       nvidia-container-toolkit.enable = true;
