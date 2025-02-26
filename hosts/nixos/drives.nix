bf676bb1bd5f3bf55157cab604b4e311a4375cff
ajhyperbit <ajhyperbit@gmail.com>
2025-02-26 16:16:56 -0600

    testing a bit of an odd mount scenario

--- hosts/nixos/drives.nix
+++ hosts/nixos/drives.nix
@@ -26,12 +26,19 @@
     ];
   };
 
-  #filesystems."/home/ajhyperbit/mnt/Archive" = {
-  #  device = "/dev/disk/by-uuid/4fd45309-e0dc-4124-8c19-36c011aad8eb"
-  #};
+  fileSystems."/home/ajhyperbit/mnt/Archive" = {
+    device = "/dev/disk/by-uuid/4fd45309-e0dc-4124-8c19-36c011aad8eb";
+    fsType = "btrfs";
+    options = [
+      "compress=zstd" #Enable default zstd compression
+    ];
+  };
 
   fileSystems."/run/media/ajhyperbit/Archive" = {
-    device = "/dev/disk/by-uuid/4fd45309-e0dc-4124-8c19-36c011aad8eb";
+    depends = [
+      "/home/ajhyperbit/mnt/Archive"
+    ];
+    device = "/home/ajhyperbit/mnt/Archive/@";
     fsType = "btrfs";
     options = [
       # If you don't have this options attribute, it'll default to "defaults"
@@ -41,8 +48,6 @@
       "exec" # Permit execution of binaries and other executable files
       "noauto" #Do not mount the filesystem automatically
       "compress=zstd" #Enable default zstd compression
-      "subvolid=258"
-      "subvol=@"
     ];
   };
 }
