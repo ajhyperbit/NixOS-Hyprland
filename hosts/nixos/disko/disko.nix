57b0f7ca7791045a6676a5a98700c5653072274f
ajhyperbit <ajhyperbit@gmail.com>
2025-03-04 11:18:59 -0600

    working on disko.nix

--- hosts/nixos/disko/disko.nix
+++ hosts/nixos/disko/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       root = {
         type = "disk";
-        #device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_500GB_S58SNM0RC03948A";
+        #device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_500GB_S58SNM0RC03948A"; #FIXME - needs disk
         content = {
           type = "gpt";
           partitions = {
@@ -54,16 +54,150 @@
         };
       };
     };
-    {
-    type = "disk";
-      device = "";
-      content = {
-        type = "gpt";
-        partitions = {
-              
+    disk = {
+      home = {
+        type = "disk";
+        #device = ""; #FIXME - needs disk
+        content = {
+          type = "gpt";
+          partitions = {
+            home = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  "@" = {};
+                  "@/home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                    ];
+                  };
+                  "@/.snapshots" = {
+                    mountOptions = [
+                      "compress=zstd"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
       };
     };
-
+    disk = {
+      DATA = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-ST2000NE0025-2FL101_ZDS1968N";
+        content = {
+          type = "gpt";
+          partitions = {
+            DATA = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "@" = {};
+                  "@/DATA" = {
+                    mountpoint = "/run/media/ajhyperbit/DATA";
+                    mountOptions = [
+                      "compress=zstd" #Enable default zstd compression
+                    ];
+                  };
+                  "@/.snapshots" = {
+                    mountOptions = [
+                      "compress=zstd" #Enable default zstd compression
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
     };
-  }
+    disk = {
+      SATASSD = {
+        type = "disk";
+        #device = "/dev/disk/by-id/ata-WDC_WDS200T2B0A_19162B802185"; #REVIEW - Check disk
+        content = {
+          type = "gpt";
+          partitions = {
+            SATASSD = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  "@" = {};
+                  "@/SATA SSD" = {
+                    mountpoint = "/run/media/ajhyperbit/SATA SSD";
+                    mountOptions = [
+                      "users" # Allows any user to mount and unmount
+                      "nofail" # Prevent system from failing if this drive doesn't mount
+                      "exec" # Permit execution of binaries and other executable files
+                      "auto" #Mount the filesystem automatically
+                      "compress=zstd" #Enable default zstd compression
+                    ];
+                  };
+                  "/.snapshots" = {
+                    mountpoint = "/run/media/ajhyperbit/SATA SSD/.snapshots";
+                    mountOptions = [
+                      "users" # Allows any user to mount and unmount
+                      "nofail" # Prevent system from failing if this drive doesn't mount
+                      "exec" # Permit execution of binaries and other executable files
+                      "auto" #Mount the filesystem automatically
+                      "compress=zstd" #Enable default zstd compression
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    disk = {
+      Archive = {
+        type = "disk";
+        #device = "/dev/disk/by-id/ata-ST6000VN0033-2EE110_ZADBCVNZ"; #FIXME - needs disk
+        content = {
+          type = "gpt";
+          partitions = {
+            Archive = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "@" = {};
+                  "@/Archive" = {
+                    mountpoint = "/run/media/ajhyperbit/Archive";
+                    mountOptions = [
+                      "compress=zstd" #Enable default zstd compression
+                    ];
+                  };
+                  "@/.snapshots" = {
+                    mountOptions = [
+                      "compress=zstd"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
